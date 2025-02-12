80b564cb5845b8381eb1091279f458ba19bcd000
ViniciusDev <viniciusdev10@gmail.com>
2025-02-12 09:36:07 -0300

    created login with back end

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,46 @@
-
+import { useRef } from "react";
+import api from '../services/api.js'
 
 
 function Login() {
+
+    const email = useRef()
+    const password = useRef()
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        try{
+            if(email.current.value === '' || password.current.value === ''){
+                alert('Preencha todos os campos')
+                return
+            }
+
+
+            const response = await api.post('/login', {
+                email : email.current.value,
+                password : password.current.value,
+            })
+
+            alert(response.data.message)
+
+        } catch (e) {
+            console.error(e)
+            alert('Falha ao fazer login, verifique seus dados')
+        }
+
+       
+    }
+
 
     return(
 
         <div>
             <h1>Login</h1>
-            <form action="">
-                <input type="email" placeholder="Email"/>
-                <input type="password" placeholder="Senha" />
+            <form action="" onSubmit={handleSubmit}>
+                <input type="email" placeholder="Email" ref={email}/>
+                <input type="password" placeholder="Senha"  ref={password}/>
+                <button>Login</button>
             </form>
         </div>
 
